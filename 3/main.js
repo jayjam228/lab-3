b919e88b98d16dc9c4c9a4999b40e494f0cee7ec
jayjam228 <93923199+jayjam228@users.noreply.github.com>
2022-02-24 22:46:01 +0600

    Add files via upload

---
+++ 3/main.js
@@ -0,0 +1,70 @@
+let min= 0;
+    let max= 100;
+
+    let middle = Math.floor((min + max)/2 );
+
+    let stage = 1;
+function start() {
+    document.getElementById("startText").classList.add("hideBlock");
+    document.getElementById("startButton").classList.add("hideBlock");
+
+    document.getElementById("question").classList.remove("hideBlock")
+    document.getElementById("question").innerHTML = "Попытка " + stage + ": " + middle
+
+    document.getElementById("small").classList.remove("hideBlock")
+    document.getElementById("bingo").classList.remove("hideBlock")
+    document.getElementById("big").classList.remove("hideBlock")
+
+    
+}
+
+function smallClick () {
+    max = middle;
+    middle = Math.floor((min + max)/2)
+    if (max == middle) {
+        middle -= 1;
+    }
+
+
+    checkGame();
+}
+
+function bigClick () {
+    min = middle;
+    middle = Math.floor((min + max)/2)
+    if (min == middle) {
+        middle += 1;
+    }
+
+
+    checkGame();
+}
+
+function bingoClick () {
+   
+    max = middle;
+    min = middle;
+
+
+    checkGame();
+}
+
+function checkGame() {
+    if (min == max) {
+        document.getElementById("question").innerHTML = "вы загадали число " + min;
+
+        document.getElementById("small").classList.add("hideBlock");
+        document.getElementById("bingo").classList.add("hideBlock");
+        document.getElementById("big").classList.add("hideBlock");
+
+        return;
+    }
+
+
+    stage += 1;
+    if (stage == 8){
+        document.getElementById("question").innerHTML = "жульничать не хорошо!"
+    }
+
+    document.getElementById("question").innerHTML = "попытка" + stage + ": " + middle;
+}
